fd0ab38d554c3672fdcd10d32f857f91e7b8a8fa
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-08-17 11:38:16 -0700

    add new arg, finding matches now faster

--- group_kos.py
+++ group_kos.py
@@ -11,6 +11,7 @@ Script to group KO identifiers into higher categories in a summary table.
 import argparse
 import pandas as pd
 import os
+import time
 from collections import defaultdict
 
 def listDirNoHidden(indir):
@@ -19,8 +20,10 @@ def listDirNoHidden(indir):
 def getArgs():
 	parser = argparse.ArgumentParser(description = "Group KO identifiers into higher categories.")
 	parser.add_argument("-k", "--ko-table", required = True, help = "KEGG Orthology csv generated from the KEGG-to-anvio script.")
-	parser.add_argument("-o", "--outpath", default = ".", help = "Path to output file.")
 	parser.add_argument("-g", "--gene-calls-dir", required = True, help = "Directory containing gene calls exported from An'vio.")
+	parser.add_argument("-4", "--include-fourth-level", default = False, help = "Specify this flag to output a table with the fourth hierarchical level. " + 
+		"The fourth level are annotations at the protein level, and takes a significant amount of time. Default is to skip the fourth level.")
+	parser.add_argument("-o", "--outpath", default = ".", help = "Path to output file.")
 	args = parser.parse_args()
 	return args
 
@@ -40,14 +43,14 @@ def formatKOTable(ko_table):
 	df["Category3"].apply(lambda x: deletePaths(x))
 	return df
 
-def getMatches(df, l):
+def getMatches(df, indices, l):
 	dd = {}
 	dd = defaultdict(lambda: 0, dd)
 	# this will take a while...
 	for ko in l:
-		for i, row in df.iterrows():
-			if ko == i:
-				for v in row.values:
+		if ko in indices:
+			for row in df.loc[ko].values:
+				for v in row:
 					dd[v] += 1
 	return dd
 
@@ -59,31 +62,46 @@ def getCategories(df):
 	return cat1, cat2, cat3, cat4
 
 def main():
+	main_t0 = time.time()
 	args = getArgs()
 	KO_TABLE = args.ko_table
-	OUTPATH = args.outpath
 	GENE_CALLS_DIR = args.gene_calls_dir
+	INCLUDE_FOURTH_LEVEL = args.include_fourth_level
+	OUTPATH = args.outpath
 	gene_calls_files = [f for f in listDirNoHidden(GENE_CALLS_DIR) if os.path.isfile(os.path.join(GENE_CALLS_DIR, f))]
 	df = formatKOTable(KO_TABLE)
-	categories = df.columns.values
+	columns = df.columns.values
+	categories = columns[:len(columns) - 1]
+	if INCLUDE_FOURTH_LEVEL:
+		categories.append(columns[-1])
 	for category in categories:
-		print "on category %s" % category
+		category_time0 = time.time()
+		print "\n"
+		print "=========================="
+		print "ON CATEGORY %s" % category
+		print "=========================="
 		outfile = open(os.path.join(OUTPATH, "kegg_" + category + ".txt"), "w")
 		outfile.write("kegg_category\t" + "\t".join(os.path.splitext(f)[0] for f in gene_calls_files) + "\n")
 		cols = df[category].unique()
-		ko_list = None
 		for col in cols:
-			print "on column %s" % col
+			print "\n"
+			print "On column %s" % col
+			col_t0 = time.time()
 			outfile.write(col + "\t")
 			for f in gene_calls_files:
-				print "working on file %s" % f
+				print "Working on file %s" % f
 				FULL_PATH = os.path.join(GENE_CALLS_DIR, f)
-				if ko_list is None:
-					ko_list = getKOList(FULL_PATH)
-				all_ko_counts = getMatches(df, ko_list)
-				outfile.write(all_ko_counts[col] + "\t")
-				print "finished writing for file %s" % f 
+				ko_list = getKOList(FULL_PATH)
+				all_ko_counts = getMatches(df, df.index.values, ko_list)
+				outfile.write(str(all_ko_counts[col]) + "\t")
+			col_t1 = time.time()
+			print "Finished writing column %s. Time taken: %f s" % (col, col_t1 - col_t0) 
 			outfile.write("\n")
+		outfile.close()
+		category_time1 = time.time()
+		print "FINISHED category %s with elapsed time: %f s" % (category, category_time1 - category_time0)
+	main_t1 = time.time()
+	print "FINISHED with overall elapsed time: %f s" % (main_t1 - main_t0)
 
 if __name__ == "__main__":
 	main()
